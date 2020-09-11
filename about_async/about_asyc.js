a21e3de8d6da9b4720e3cda1226f0907c0610a0d
kata-n <kata.kawa813@gmail.com>
2020-09-11 21:00:14 +0900

    awaitとasyncについて

--- about_async/about_asyc.js
+++ about_async/about_asyc.js
@@ -14,3 +14,25 @@ sleep(function () {
 new Promise(function (resolve, reject) {
   console.log('promise');
 })
+
+//AwaitとAsync
+//AsyncはPromiseを返却する関数の宣言を行う
+//AwaitはPromiseを返却する関数の非同期処理が完了するまで待つ
+function sleep(callback_value) {
+  return new Promise(function (resolve) {
+    setTimeout(() => {
+      console.log(callback_value++);
+      resolve(callback_value);
+    }, 2000);
+  });
+}
+//async関数のなかでswaitを使う
+async function init() {
+  let callvalue = await sleep(0);
+  callvalue = await sleep(callvalue);
+  callvalue = await sleep(callvalue);
+  callvalue = await sleep(callvalue);
+  console.log(callvalue);
+}
+
+init();
