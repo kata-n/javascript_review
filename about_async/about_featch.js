31cbe8f7284fadb5af342ebad77df87a73c5da3f
kata-n <kata.kawa813@gmail.com>
2020-09-17 22:53:36 +0900

    例外処理について

--- about_async/about_featch.js
+++ about_async/about_featch.js
@@ -13,4 +13,19 @@ async function getUsers() {
 getUsers();
 
 
-//例外処理
+//例外処理の書き方
+async function fetchUsers() {
+  const response = await fetch('users.json');
+  if (response.ok) {
+    const json = await response.json();
+    return json;
+  }
+}
+
+async function init() {
+  const users = await fetchUsers();
+  for (const user of users) {
+    console.log(`${user.name},${user.age}`)
+  }
+}
+init();
