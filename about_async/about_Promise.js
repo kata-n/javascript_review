52e978fb3381c8575cd92484a03d96a7914e0c6d
河村 泰佑 <kata.kawa813@gmail.com>
2020-11-01 21:38:11 +0900

    Promise

---
+++ about_async/about_Promise.js
@@ -0,0 +1,14 @@
+//Promise 非同期処理、並列で処理したい時に使う
+function sleep(val) {
+  return new Promise(function (resolve) {
+    setTimeout(function() {
+      console.log(val++);
+      resolve(val);
+    }, val * 500);
+  });
+}
+
+Promise.all([sleep(2)])
+sleep(0).then(function (val) {
+  return sleep(val);
+});
