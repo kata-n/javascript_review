2f2f7367e12d58959931476105194c403a8efad5
河村 泰佑 <kata.kawa813@gmail.com>
2020-11-08 20:12:36 +0900

    Promiseについて

--- about_async/about_Promise.js
+++ about_async/about_Promise.js
@@ -2,26 +2,31 @@
 //5秒後にhelloを出力してからworldを出力
 const promise = new Promise((resolve, reject) => {
   setTimeout(() => {
-    console.log('hello');
+    console.log("hello");
+    //resolve関数で終了した事を明示する
     resolve();
   }, 5000);
 });
 
-promise.then(() => console.log('world!'));
+//promiseにはthenメソッドがあり、実行が終わった後にする処理を記述できる
+promise.then(() => console.log("world!"));
 
+//-------------------------------------------------------
 
 function sleep(val) {
   return new Promise(function (resolve) {
-    setTimeout(function() {
+    setTimeout(function () {
       console.log(val++);
       resolve(val);
     }, val * 500);
   });
 }
 
-Promise.all([sleep(2)])
-sleep(0).then(function (val) {
-  return sleep(val);
-}).then(function (val) {
-  return sleep(val);
-});
+Promise.all([sleep(2)]);
+sleep(0)
+  .then(function (val) {
+    return sleep(val);
+  })
+  .then(function (val) {
+    return sleep(val);
+  });
